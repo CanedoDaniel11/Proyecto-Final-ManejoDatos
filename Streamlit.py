54cd9569e079746faed011e31c2b925845e20fda
Daniel Canedo <da.canedo27@ciencias.unam.mx>
2023-05-30 01:40:34 -0600

    introducción

--- Streamlit.py
+++ Streamlit.py
@@ -1,3 +1,18 @@
-# Se generará un cambio en git?
-# al parecer ya se pueden hacer cambios desde vscode y se tambien se actualiza en github
-#mas comentarios
+#comenzamos instalando streamlit
+import streamlit as st
+
+st.write("""
+# Predicción de ventas
+
+En este proyecto tomaremos los registro de venta de limonadas de una base de datos y a través de redes 
+neuronales generaremos un modelo que nos permita predecir de una manera efectiva el comportamiento de 
+las proximas ventas.
+
+
+El proyecto ya se encuentra de manera completa en la siguiente liga: [Proyecto](https://github.com/CanedoDaniel11/Proyecto-Final-ManejoDatos/blob/master/Proyecto%20limonadas.ipynb)
+
+Nuestro objetivo es darle una visualización dinamica al entendimiento del proceso que lleva al fin del 
+modelo predictorio
+""".format())
+
+
